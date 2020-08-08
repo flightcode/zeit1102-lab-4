5c24f40be6e25446bc751b05189310a461f5d4b3
Elliot Parker <elliotccp@protonmail.com>
2020-08-08 11:53:52 +1000

    Add second cyclist

--- TeamDriver.java
+++ TeamDriver.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class TeamDriver {
     Cyclist c1;
+    Cyclist c2;
     Scanner scan = new Scanner(System.in);
 
     public void readCyclist1() {
@@ -10,6 +11,11 @@ public class TeamDriver {
         System.out.print("Enter the first cyclist team : ");
         String team = scan.nextLine();
         c1 = new Cyclist(name, team);
+        System.out.print("Enter the second cyclist name : ");
+        name = scan.nextLine();
+        System.out.print("Enter the second cyclist team : ");
+        team = scan.nextLine();
+        c2 = new Cyclist(name, team);
     }
 
     public void doMainLoop() {
